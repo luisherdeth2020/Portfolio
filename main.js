bf44f8641e622f530dfab42b5e1e5e2dc963e64a
luisherdeth2020 <73970555+luisherdeth2020@users.noreply.github.com>
2022-01-13 15:12:09 +0100

    add  dark theme
    change theme

--- main.js
+++ main.js
@@ -4,44 +4,44 @@ const navClose = document.getElementById('nav-close');
 
 /**------------------------------------------------------------------------
  * *                                SHOW MENU
- *   
- *   
- *   
+ *
+ *
+ *
  *
  *------------------------------------------------------------------------**/
 
-if(navOpen){
-
-    navOpen.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-    });
+if (navOpen) {
+	navOpen.addEventListener('click', () => {
+		navMenu.classList.add('show-menu');
+	});
 }
 
-if(navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    })
+if (navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu');
+	});
 }
 
 /**------------------------------------------------------------------------
  * *                                REMOVE MENU MOBILE
- *   
- *   
- *   
+ *
+ *
+ *
  *
  *------------------------------------------------------------------------**/
 
 const navLink = document.querySelectorAll('.nav__link');
 
-    navLink.forEach(n => n.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
-}) )
-
+navLink.forEach((n) =>
+	n.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu');
+	})
+);
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll('section[id]');
 
-function scrollActive(){
+function scrollActive() {
 	// devuelve los píxeles del documento actual se ha desplazado
 	const scrollY = window.pageYOffset;
 
@@ -63,9 +63,65 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-// function scrollHeader(){
-//     const nav = document.getElementById('header');
-//     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-//     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+// setInterval(() => {
+	
+// 	alert(this.scrollY + 'num')
+// }, 3000);
+// function scrollHeader() {
+// 	const scrollNav = document.getElementById('header');
+// 	// When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+// 	if (this.scrollY >= 80) {
+// 		scrollNav.classList.add('scroll-header');
+// 	} else {
+// 		scrollNav.classList.remove('scroll-header');
+// 	}
 // }
 // window.addEventListener('scroll', scrollHeader);
+
+window.addEventListener('scroll',()=>{
+	const scrollNav = document.getElementById('header');
+	// When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+	if (scrollY >= 80) {
+		scrollNav.classList.add('scroll-header');
+	} else {
+		scrollNav.classList.remove('scroll-header');
+	}
+})
+
+/**========================================================================
+ *                             THEME DARK
+ *  
+ *  
+ *  
+ *  
+ *========================================================================**/
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
