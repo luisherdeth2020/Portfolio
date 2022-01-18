4d3f2e0e68719f89ad0bcdb43100c0f027f75fe6
luisherdeth2020 <73970555+luisherdeth2020@users.noreply.github.com>
2022-01-18 15:59:08 +0100

    add bar
    add js, progress bar skill

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ window.addEventListener('scroll', scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 // setInterval(() => {
-	
+
 // 	alert(this.scrollY + 'num')
 // }, 3000);
 // function scrollHeader() {
@@ -78,7 +78,7 @@ window.addEventListener('scroll', scrollActive);
 // }
 // window.addEventListener('scroll', scrollHeader);
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
 	const scrollNav = document.getElementById('header');
 	// When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
 	if (scrollY >= 80) {
@@ -86,42 +86,66 @@ window.addEventListener('scroll',()=>{
 	} else {
 		scrollNav.classList.remove('scroll-header');
 	}
-})
+});
 
 /**========================================================================
- *                             THEME DARK
- *  
- *  
- *  
- *  
+ *                             Dark theme
+ *
+ *
+ *
+ *
  *========================================================================**/
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
+const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun');
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+	// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+	themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
 }
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+	// Add or remove the dark / icon theme
+	document.body.classList.toggle(darkTheme);
+	themeButton.classList.toggle(iconTheme);
+	// We save the theme and the current icon that the user chose
+	localStorage.setItem('selected-theme', getCurrentTheme());
+	localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+// barraaaaa
+
+window.addEventListener('scroll', () => {
+	let currentPosition = document.documentElement.scrollTop;
+
+	const elements = {
+		scrollDown: {
+			// classes for scroll down, id is on the left and class is on the right
+			skillHtml: 'skills__react',
+			skillCss: 'skills__react',
+			skillJs: 'skills__react',
+		},
+	};
+	// Object.keys => Devuelve en array
+	Object.keys(elements.scrollDown).forEach((idBar) => {
+		//offsetTop => retorna distancia del elemento actual respecto al borde superior
+		if (document.getElementById(idBar).offsetTop - 800 < currentPosition) {
+			document.getElementById(idBar).classList.add(elements.scrollDown[idBar]);
+		} else {
+			document.getElementById(idBar).classList.remove(elements.scrollDown[idBar]);
+		}
+	});
+});
