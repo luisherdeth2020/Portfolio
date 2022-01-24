a76a946e43afca277d65637402ccaa7116e5a7e4
luisherdeth2020 <73970555+luisherdeth2020@users.noreply.github.com>
2022-01-24 21:41:24 +0100

    change theme, icons

--- main.js
+++ main.js
@@ -97,9 +97,11 @@ window.addEventListener('scroll', () => {
  *========================================================================**/
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button');
 const darkTheme = 'dark-theme';
-const iconTheme = 'uil-sun';
+// const iconTheme = 'uil-sun';
+
+const uilMoon = document.getElementById('uil-moon');
+const uilSun = document.getElementById('uil-sun');
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme');
@@ -107,20 +109,43 @@ const selectedIcon = localStorage.getItem('selected-icon');
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
-const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun');
+
+// const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun');
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
 	// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
 	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-	themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+	// themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+} else {
+	document.body.classList.add(darkTheme);
+}
+function showIcon() {
+	const currentTheme = getCurrentTheme();
+	if (currentTheme === 'light') {
+		uilMoon.classList.toggle('hide');
+		uilSun.classList.toggle('hide');
+	}
 }
 
+showIcon();
+
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
+uilMoon.addEventListener('click', () => {
 	// Add or remove the dark / icon theme
 	document.body.classList.toggle(darkTheme);
-	themeButton.classList.toggle(iconTheme);
+	uilMoon.classList.toggle('hide');
+	uilSun.classList.toggle('hide');
+	// We save the theme and the current icon that the user chose
+	localStorage.setItem('selected-theme', getCurrentTheme());
+	localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+uilSun.addEventListener('click', () => {
+	// Add or remove the dark / icon theme
+	document.body.classList.toggle(darkTheme);
+	uilMoon.classList.toggle('hide');
+	uilSun.classList.toggle('hide');
 	// We save the theme and the current icon that the user chose
 	localStorage.setItem('selected-theme', getCurrentTheme());
 	localStorage.setItem('selected-icon', getCurrentIcon());
@@ -142,7 +167,7 @@ window.addEventListener('scroll', () => {
 			skillsTailwind: 'skills__tailwind',
 			skillsReact: 'skills__react',
 			skillsGit: 'skills__git',
-			skillsFigma:'skills__figma',
+			skillsFigma: 'skills__figma',
 			skillsXd: 'skills__xd',
 			skillsPhotoshop: 'skills__photoshop',
 		},
@@ -177,10 +202,10 @@ skillsHeader.forEach((skill) => {
 
 /**----------------------------------------------
  *                SLIDER
- *  
- *  
- *  
- *  
+ *
+ *
+ *
+ *
  *---------------------------------------------**/
 
 const grande = document.querySelector('.grande');
@@ -219,7 +244,7 @@ puntito.forEach((cadaPunto, i) => {
 		grande.style.transform = `translateX(${operacion}%)`;
 
 		puntito.forEach((cadaPunto, i) => {
-			console.log(puntito)
+			console.log(puntito);
 			punto[i].classList.remove('activo');
 		});
 		punto[i].classList.add('activo');
